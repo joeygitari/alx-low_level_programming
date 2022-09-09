f4f3e71d432ad9e9658986bd7d90ccd250d31f78
joeygitari <gitarijoanne@gmail.com>
2022-09-09 15:06:03 +0300

    New files

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int t;
+	int o;
+
+	for (t = 0; t <= 9; t++)
+	{
+		for(o = t + 1; o <= 9; o++)
+		{
+			putchar(t + '0');
+			putchar(o + '0');
+
+			if(t < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return(0);
+}
+
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b;
+
+	for(a = 0; a < 99; a++)
+	{
+		for(b = a + 1; b < 100; b++)
+		{
+			putchar(a / 10 + '0');
+			putchar(a % 10 + '0');
+			putchar(' ');
+			putchar(b / 10 + '0');
+			putchar(b % 10 + '0');
+
+			if (a != 98 || b != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return(0);
+}
+
+
