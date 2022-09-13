03442cc474e7089908a56cdf2af333c1c8fe8252
joeygitari <gitarijoanne@gmail.com>
2022-09-13 19:04:46 +0300

    functions, nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "main.h"
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+			{
+				continue;
+			}
+			printf(", ");
+		}
+		printf("\n");
+
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+
+			if(n == 98)
+			{
+				continue;
+			}
+			printf(", ");
+		}
+		printf("\n");
+	}
+}
+
