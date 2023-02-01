fc6595db93794ad4ec5984ce43fb4ec5e1900831
joeygitari <gitarijoanne@gmail.com>
2023-02-01 06:31:26 -0800

    singly linked

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ *  * print_listint - print data of linked list
+ *   * @h: points to the head of list
+ *    * Return: number of nodes
+ *     */
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *temp;
+	unsigned int i = 0;
+
+	temp = h;
+	for (; temp; i++)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	
+	}
+
+	return (i);
+}
